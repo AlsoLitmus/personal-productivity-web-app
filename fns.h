6c1c085200ec40db50eecb1fa266bff2883acc5f
Alisa Tkachenko <155114764+AlsoLitmus@users.noreply.github.com>
2025-12-27 22:07:33 -0800

    Add markComplete function to fns.h
    Added markComplete function declaration to fns.h

--- fns.h
+++ fns.h
@@ -13,8 +13,7 @@ int getChoice(std::vector<std::vector<std::string>> &tasks);
 void createTask(std::vector<std::vector<std::string>> &tasks);
 void editTask(std::vector<std::vector<std::string>> &tasks);
 void deleteTask(std::vector<std::vector<std::string>> &tasks);
+void markComplete(std::vector<std::vector<std::string>>& tasks);
 void viewTasks(std::vector<std::vector<std::string>> &tasks);
 
 #endif
-
-
